696b1951db423519f50c0b4aec64dd9dac561ccc
nazarenof <nazarenof@users.noreply.github.com>
2018-11-23 22:35:54 +0000

    Add count_instances probe
    Add tests for count_instances.
    Update README.md.
    Improve clarity in actions (mock naming).
    Add space to inline noqa comment.
    Update CHANGELOG.md.
    
    Signed-off-by: nazarenof <nazarenof@users.noreply.github.com>

--- tests/compute/test_compute_actions.py
+++ tests/compute/test_compute_actions.py
@@ -6,10 +6,10 @@ from chaosoci.compute.actions import stop_instance
 
 @patch('chaosoci.compute.actions.oci_client', autospec=True)
 def test_stop_instance(oci_client):
-    client = MagicMock()
-    oci_client.return_value = client
+    compute_client = MagicMock()
+    oci_client.return_value = compute_client
     inst_id = "i-1234567890abcdef0"
     action = "SOFTSTOP"
     stop_instance(inst_id, action)
-    client.instance_action.assert_called_with(instance_id=inst_id,
-                                              action=action)
+    compute_client.instance_action.assert_called_with(instance_id=inst_id,
+                                                      action=action)
